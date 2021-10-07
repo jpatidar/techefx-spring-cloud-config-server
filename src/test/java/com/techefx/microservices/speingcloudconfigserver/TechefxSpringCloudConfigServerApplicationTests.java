dd5a6877ba285ccf229b80532f5f474cde393b6e
jpatidar <jpatidar@gmail.com>
2021-10-06 22:03:14 -0400

    test project

---
+++ src/test/java/com/techefx/microservices/speingcloudconfigserver/TechefxSpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techefx.microservices.speingcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechefxSpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
